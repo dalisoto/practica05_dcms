72eb6b653436df20045b16e3b59a67b07a1f950d
dalisoto <daliadelcarmenmendiolasoto@gmail.com>
2025-06-16 20:17:24 -0600

    main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './Layout';
 import './App.css';
+
+function Home() {
+  return <h1 style={{textAlign:'center'}}>Bienvenidos a la Página de Inicio</h1>;
+}
+function About() {
+  return (
+  <div>
+    <h2>Acerca de Nosotros</h2>
+    <h3>Hola soy Dalia del Carmen Mendiola Soto</h3>
+  </div>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
